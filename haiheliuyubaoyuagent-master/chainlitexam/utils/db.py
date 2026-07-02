0c8d8d72fa03646e15a84368fc65aa352578b55f
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-02 17:39:38 +0800

    fix: initialize last month areal route from imported db module

--- haiheliuyubaoyuagent-master/chainlitexam/utils/db.py
+++ haiheliuyubaoyuagent-master/chainlitexam/utils/db.py
@@ -3,6 +3,13 @@ from sqlalchemy.orm import sessionmaker
 
 from utils.config import DB_CONFIG
 
+try:
+    from last_month_areal_patch import install_last_month_areal_patch as _load_last_month_areal_route
+
+    _load_last_month_areal_route()
+except Exception as exc:
+    print(f"[utils.db] last month areal route init failed: {exc}")
+
 engine = create_engine(
     f"postgresql+psycopg2://{DB_CONFIG['user']}:{DB_CONFIG['password']}"
     f"@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}"
