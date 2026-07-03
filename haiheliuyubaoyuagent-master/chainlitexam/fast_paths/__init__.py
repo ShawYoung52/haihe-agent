fcdea8ff1f7c0ddc0615fa1c98bb1348c7d64cfc
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-03 11:21:47 +0800

    revert: stop installing areal fallback patch

--- haiheliuyubaoyuagent-master/chainlitexam/fast_paths/__init__.py
+++ haiheliuyubaoyuagent-master/chainlitexam/fast_paths/__init__.py
@@ -1,13 +1,6 @@
 """Chainlit 前端自然语言快速路径统一入口。"""
 from __future__ import annotations
 
-from .rainfall_fast_paths import install_all_fast_paths as _install_rainfall_fast_paths
-from .areal_fallback_patch import install_areal_fallback_patch
-
-
-def install_all_fast_paths() -> None:
-    _install_rainfall_fast_paths()
-    install_areal_fallback_patch()
-
+from .rainfall_fast_paths import install_all_fast_paths
 
 __all__ = ["install_all_fast_paths"]
