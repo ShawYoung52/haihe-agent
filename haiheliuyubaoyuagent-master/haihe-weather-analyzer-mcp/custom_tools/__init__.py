95695a4678c5bab458e134946e607913a42fd70f
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-03 14:03:56 +0800

    revert: stop exporting flash flood risk tool

--- haiheliuyubaoyuagent-master/haihe-weather-analyzer-mcp/custom_tools/__init__.py
+++ haiheliuyubaoyuagent-master/haihe-weather-analyzer-mcp/custom_tools/__init__.py
@@ -5,12 +5,10 @@ from .last_month_areal_rainfall_tool import register_last_month_areal_rainfall_t
 from .last_year_max_daily_rainfall_tool import register_last_year_max_daily_rainfall_tool
 from .historical_same_period_rainfall_tool import register_historical_same_period_rainfall_tool
 from .year_to_date_areal_rainfall_tool import register_year_to_date_areal_rainfall_tool
-from .flash_flood_risk_tool import register_flash_flood_risk_tool
 
 __all__ = [
     "register_last_month_areal_rainfall_tool",
     "register_last_year_max_daily_rainfall_tool",
     "register_historical_same_period_rainfall_tool",
     "register_year_to_date_areal_rainfall_tool",
-    "register_flash_flood_risk_tool",
 ]
