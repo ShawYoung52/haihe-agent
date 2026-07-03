6c806c3b6b3d66bdbe1097d645bc607b06471a31
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-03 14:16:26 +0800

    feat: initialize POI weather fast path

--- haiheliuyubaoyuagent-master/chainlitexam/fast_paths/__init__.py
+++ haiheliuyubaoyuagent-master/chainlitexam/fast_paths/__init__.py
@@ -3,11 +3,13 @@ from __future__ import annotations
 
 from .rainfall_fast_paths import install_all_fast_paths as _install_rainfall_fast_paths
 from .water_level_fast_paths import install_water_level_fast_paths
+from .poi_weather_fast_paths import install_poi_weather_fast_paths
 
 
 def install_all_fast_paths() -> None:
     _install_rainfall_fast_paths()
     install_water_level_fast_paths()
+    install_poi_weather_fast_paths()
 
 
 __all__ = ["install_all_fast_paths"]
