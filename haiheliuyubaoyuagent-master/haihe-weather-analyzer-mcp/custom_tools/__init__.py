7f8e2b78c928f5a8c77ff1a240e5c6b901504bb9
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-03 11:23:17 +0800

    revert: stop exporting generic period areal tool

--- haiheliuyubaoyuagent-master/haihe-weather-analyzer-mcp/custom_tools/__init__.py
+++ haiheliuyubaoyuagent-master/haihe-weather-analyzer-mcp/custom_tools/__init__.py
@@ -5,12 +5,10 @@ from .last_month_areal_rainfall_tool import register_last_month_areal_rainfall_t
 from .last_year_max_daily_rainfall_tool import register_last_year_max_daily_rainfall_tool
 from .historical_same_period_rainfall_tool import register_historical_same_period_rainfall_tool
 from .year_to_date_areal_rainfall_tool import register_year_to_date_areal_rainfall_tool
-from .period_areal_rainfall_tool import register_period_areal_rainfall_tool
 
 __all__ = [
     "register_last_month_areal_rainfall_tool",
     "register_last_year_max_daily_rainfall_tool",
     "register_historical_same_period_rainfall_tool",
     "register_year_to_date_areal_rainfall_tool",
-    "register_period_areal_rainfall_tool",
 ]
