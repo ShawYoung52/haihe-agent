e4f1aba317d23f1c0b89385e4d688f29f32d4708
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-04 16:19:28 +0800

    feat: initialize risk warning fast path

--- haiheliuyubaoyuagent-master/chainlitexam/fast_paths/__init__.py
+++ haiheliuyubaoyuagent-master/chainlitexam/fast_paths/__init__.py
@@ -4,12 +4,14 @@ from __future__ import annotations
 from .rainfall_fast_paths import install_all_fast_paths as _install_rainfall_fast_paths
 from .water_level_fast_paths import install_water_level_fast_paths
 from .poi_weather_fast_paths import install_poi_weather_fast_paths
+from .risk_warning_fast_paths import install_risk_warning_fast_paths
 
 
 def install_all_fast_paths() -> None:
     _install_rainfall_fast_paths()
     install_water_level_fast_paths()
     install_poi_weather_fast_paths()
+    install_risk_warning_fast_paths()
 
 
 __all__ = ["install_all_fast_paths"]
