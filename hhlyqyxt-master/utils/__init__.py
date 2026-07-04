b0df555fd4d98cd5b7d915de82edb45c545300c0
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-04 18:09:08 +0800

    feat: export realtime rainstorm impact map service

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -7,10 +7,16 @@ from .rainstorm_impact_map_service import (
     create_rainstorm_impact_thematic_map_from_station_records,
     get_rainstorm_impact_thematic_map_style,
 )
+from .rainstorm_impact_realtime_service import (
+    create_rainstorm_impact_thematic_map_from_realtime,
+    fetch_haihe_rainfall_station_records_from_realtime,
+)
 
 __all__ = [
     "build_rainstorm_impact_thematic_map_data",
     "create_rainstorm_impact_thematic_map",
     "create_rainstorm_impact_thematic_map_from_station_records",
     "get_rainstorm_impact_thematic_map_style",
+    "create_rainstorm_impact_thematic_map_from_realtime",
+    "fetch_haihe_rainfall_station_records_from_realtime",
 ]
