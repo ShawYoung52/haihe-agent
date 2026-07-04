e7ea29ceeb17f5aba0d16313e0934175bee2f17c
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-04 17:46:55 +0800

    feat: export rainstorm impact map style

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -4,9 +4,11 @@ from __future__ import annotations
 from .rainstorm_impact_map_service import (
     build_rainstorm_impact_thematic_map_data,
     create_rainstorm_impact_thematic_map,
+    get_rainstorm_impact_thematic_map_style,
 )
 
 __all__ = [
     "build_rainstorm_impact_thematic_map_data",
     "create_rainstorm_impact_thematic_map",
+    "get_rainstorm_impact_thematic_map_style",
 ]
