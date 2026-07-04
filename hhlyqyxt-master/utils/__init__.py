337ad0b635cb1eb20f581345cbef80c43d6ff79b
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-04 18:12:56 +0800

    refactor: simplify rainstorm impact utils exports

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -1,22 +1,12 @@
-"""hhlyqyxt 工具方法统一导出。"""
+"""hhlyqyxt 对外工具入口。"""
 from __future__ import annotations
 
 from .rainstorm_impact_map_service import (
-    build_rainstorm_impact_thematic_map_data,
-    create_rainstorm_impact_thematic_map,
-    create_rainstorm_impact_thematic_map_from_station_records,
-    get_rainstorm_impact_thematic_map_style,
-)
-from .rainstorm_impact_realtime_service import (
-    create_rainstorm_impact_thematic_map_from_realtime,
-    fetch_haihe_rainfall_station_records_from_realtime,
+    create_rainstorm_impact_map,
+    get_rainstorm_impact_map_style,
 )
 
 __all__ = [
-    "build_rainstorm_impact_thematic_map_data",
-    "create_rainstorm_impact_thematic_map",
-    "create_rainstorm_impact_thematic_map_from_station_records",
-    "get_rainstorm_impact_thematic_map_style",
-    "create_rainstorm_impact_thematic_map_from_realtime",
-    "fetch_haihe_rainfall_station_records_from_realtime",
+    "create_rainstorm_impact_map",
+    "get_rainstorm_impact_map_style",
 ]
