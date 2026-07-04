57f80ff736fdc15c0600609592ba5ee00afd60a8
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-04 18:52:27 +0800

    fix: expose only rainstorm impact public service

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -1,17 +1,12 @@
 """hhlyqyxt 对外工具入口。"""
 from __future__ import annotations
 
-from .rainfall_impact_geojson import (
-    build_rain24h_impact_river_geojson,
-    build_rainstorm_impact_thematic_map,
-    geojson_to_plot_segments,
+from .rainstorm_impact_map_service import (
+    create_rainstorm_impact_map,
+    get_rainstorm_impact_map_style,
 )
-from .rainstorm_impact_map_service import create_rainstorm_impact_map, get_rainstorm_impact_map_style
 
 __all__ = [
-    "build_rain24h_impact_river_geojson",
-    "build_rainstorm_impact_thematic_map",
-    "geojson_to_plot_segments",
     "create_rainstorm_impact_map",
     "get_rainstorm_impact_map_style",
 ]
