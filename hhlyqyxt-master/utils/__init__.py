abe1798605d7d8fe9363bf3a39ac2b11609047f4
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-04 18:29:21 +0800

    fix: export restored rainstorm map service

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -6,9 +6,12 @@ from .rainfall_impact_geojson import (
     build_rainstorm_impact_thematic_map,
     geojson_to_plot_segments,
 )
+from .rainstorm_impact_map_service import create_rainstorm_impact_map, get_rainstorm_impact_map_style
 
 __all__ = [
     "build_rain24h_impact_river_geojson",
     "build_rainstorm_impact_thematic_map",
     "geojson_to_plot_segments",
+    "create_rainstorm_impact_map",
+    "get_rainstorm_impact_map_style",
 ]
