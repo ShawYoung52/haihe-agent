188ed66ac0c74e9a1463edc2d1478b22a76f0d54
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-05 10:10:37 +0800

    refactor: expose only rainstorm impact url parser

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -1,16 +1,6 @@
 """hhlyqyxt 对外工具入口。"""
 from __future__ import annotations
 
-from .rainstorm_impact_map_service import (
-    build_rainstorm_impact_map_from_package,
-    create_rainstorm_impact_geojson_file,
-    create_rainstorm_impact_map,
-    get_rainstorm_impact_map_style,
-)
+from .rainstorm_impact_map_service import build_rainstorm_impact_map_from_url
 
-__all__ = [
-    "build_rainstorm_impact_map_from_package",
-    "create_rainstorm_impact_geojson_file",
-    "create_rainstorm_impact_map",
-    "get_rainstorm_impact_map_style",
-]
+__all__ = ["build_rainstorm_impact_map_from_url"]
