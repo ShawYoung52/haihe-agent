e31249d4cd0e415f7b69e9ad8a4dac9f1459681a
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-05 10:02:39 +0800

    feat: export rainstorm impact package parser

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -2,12 +2,14 @@
 from __future__ import annotations
 
 from .rainstorm_impact_map_service import (
+    build_rainstorm_impact_map_from_package,
     create_rainstorm_impact_geojson_file,
     create_rainstorm_impact_map,
     get_rainstorm_impact_map_style,
 )
 
 __all__ = [
+    "build_rainstorm_impact_map_from_package",
     "create_rainstorm_impact_geojson_file",
     "create_rainstorm_impact_map",
     "get_rainstorm_impact_map_style",
