6b3718e9d94fae0e548e243d4bf23f63fdfd0d11
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-05 11:13:37 +0800

    restore: export rainstorm impact generation and storage helpers

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -1,6 +1,16 @@
 """hhlyqyxt 对外工具入口。"""
 from __future__ import annotations
 
-from .rainstorm_impact_map_service import get_rainstorm_impact_json_urls_from_url
+from .rainstorm_impact_map_service import (
+    create_rainstorm_impact_geojson_file,
+    create_rainstorm_impact_map,
+    get_rainstorm_impact_json_urls_from_url,
+    get_rainstorm_impact_map_style,
+)
 
-__all__ = ["get_rainstorm_impact_json_urls_from_url"]
+__all__ = [
+    "create_rainstorm_impact_geojson_file",
+    "create_rainstorm_impact_map",
+    "get_rainstorm_impact_json_urls_from_url",
+    "get_rainstorm_impact_map_style",
+]
