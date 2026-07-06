3c93805ebd3a06993d0c96ac8a68fae82cd3243e
ShawYoung52 <xiao2198521365@gmail.com>
2026-07-06 14:34:21 +0800

    refactor: remove rainstorm impact monkey patch installer

--- hhlyqyxt-master/utils/__init__.py
+++ hhlyqyxt-master/utils/__init__.py
@@ -1,10 +1,6 @@
 """hhlyqyxt 对外工具入口。"""
 from __future__ import annotations
 
-from .rainfall_impact_graph_start_rules import install_strict_direct_part_start_rule
-
-install_strict_direct_part_start_rule()
-
 from .rainstorm_impact_map_service import (
     create_rainstorm_impact_geojson_file,
     create_rainstorm_impact_map,
